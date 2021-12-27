59a3f5c2824ba25b9d5e74f0cc30b77820c3d951
Vinicius Gonçalves de Oliveira <vinicius@myriaddev.me>
2021-12-26 21:49:48 -0300

    feat: config follows metatable

--- lua/header42.lua
+++ lua/header42.lua
@@ -6,68 +6,16 @@
 --   By: emendes- <emendes-@students.42sp.org.br>   +#+  +:+       +#+        --
 --                                                +#+#+#+#+#+   +#+           --
 --   Created: 2021/05/15 11:51:46 by emendes-          #+#    #+#             --
---   Updated: 2021/12/15 22:35:49 by vgoncalv         ###   ########.fr       --
+--   Updated: 2021/12/26 19:34:34 by vgoncalv         ###   ########.fr       --
 --                                                                            --
 -- -------------------------------------------------------------------------- --
 
 local M = {}
 
-local config
-local utils = require("header42.utils")
-local bo = vim.bo
-local api = vim.api
+local config = require('header42.config')
 
 M.setup = function(opts)
-	config = require("header42.config"):set(opts)
+	config.setup(opts)
 end
 
---- Global function for command!
-_G.Stdheader = function()
-	local filetype = bo.filetype
-	-- Error handling
-	if config.ft[filetype] == nil then
-		utils.error(string.format("Filetype '%s' is not registered", filetype))
-		return
-	else
-		local fields = { "start_comment", "end_comment", "fill_comment" }
-		for _, field in ipairs(fields) do
-			if config.ft[filetype][field] == nil then
-				utils.error(string.format("Missing required field for filetype '%s': %s", filetype, field))
-				return
-			end
-		end
-	end
-	local header = require("header42.header")
-	local ft_config = config.ft[filetype]
-	-- If header was not updated, insert it
-	if not header.update(ft_config) then
-		header.insert(ft_config)
-	end
-end
-
-_G.Stdheader_update = function()
-	local filetype = bo.filetype
-	-- Error handling
-	if config.ft[filetype] == nil then
-		return
-	else
-		local fields = { "start_comment", "end_comment", "fill_comment" }
-		for _, field in ipairs(fields) do
-			if config.ft[filetype][field] == nil then
-				utils.warn(
-					string.format(
-						"Could not update. Reason: Missing required field for filetype '%s': %s",
-						filetype,
-						field
-					)
-				)
-				return
-			end
-		end
-	end
-	local ft_config = config.ft[filetype]
-	require("header42.header").update(ft_config)
-end
-api.nvim_command([[command! Stdheader lua Stdheader()]])
-api.nvim_command([[autocmd BufWritePre * lua Stdheader_update()]])
 return M
--- lua/header42/config.lua
+++ lua/header42/config.lua
@@ -6,66 +6,85 @@
 --   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        --
 --                                                +#+#+#+#+#+   +#+           --
 --   Created: 2021/09/12 20:57:40 by vgoncalv          #+#    #+#             --
---   Updated: 2021/12/15 22:16:55 by vgoncalv         ###   ########.fr       --
+--   Updated: 2021/12/26 19:31:23 by vgoncalv         ###   ########.fr       --
 --                                                                            --
 -- -------------------------------------------------------------------------- --
 
----@class Config User config defaults
----@field user string: default user
----@field mail string: default mail
----@field ft table: filetype configuration
-local Config = {
-	user = "marvin",
-	mail = "marvin@42.fr",
+local M = {}
+
+local bo = vim.bo
+
+M.config = M.config or {}
+
+-- Default configuration, filetypes cannot be overwritten
+local default_config = {
+	intra_login = 'marvin',
+	intra_mail = 'marvin@42.fr',
 	ft = {
 		c = {
-			start_comment = "/*",
-			end_comment = "*/",
-			fill_comment = "*",
+			start_comment = '/*',
+			end_comment = '*/',
+			fill_comment = '*',
 		},
 		cpp = {
-			start_comment = "/*",
-			end_comment = "*/",
-			fill_comment = "*",
+			start_comment = '/*',
+			end_comment = '*/',
+			fill_comment = '*',
 		},
 		make = {
-			start_comment = "##",
-			end_comment = "##",
-			fill_comment = "#",
+			start_comment = '##',
+			end_comment = '##',
+			fill_comment = '#',
 		},
 		python = {
-			start_comment = "##",
-			end_comment = "##",
-			fill_comment = "#",
+			start_comment = '##',
+			end_comment = '##',
+			fill_comment = '#',
 		},
 		lua = {
-			start_comment = "--",
-			end_comment = "--",
-			fill_comment = "-",
+			start_comment = '--',
+			end_comment = '--',
+			fill_comment = '-',
 		},
 		vim = {
 			start_comment = '""',
 			end_comment = '""',
-			fill_comment = "*",
+			fill_comment = '*',
 		},
 	},
 }
 
-setmetatable(Config, {
-	__call = function(cls, ...)
-		return cls:new(...)
-	end,
-})
-
 ---@param opts table
-function Config:set(opts)
-	self.__index = self
-	self.user = opts.user or "marvin"
-	self.mail = opts.user or "42.fr"
-	if opts.ft ~= nil then
-		self.ft = vim.tbl_deep_extend("force", self.ft, opts.ft)
-	end
-	return self
+function M.setup(opts)
+	M.config = setmetatable(opts or {}, {
+		__index = default_config,
+	})
+	return M.config
 end
 
-return Config
+---@return string: intra login
+function M.intra_login()
+	return M.config.intra_login
+end
+
+---@return string: intra mail
+function M.intra_mail()
+	return M.config.intra_mail
+end
+
+---@return string: start comment
+function M.start_comment()
+	return M.config.ft[bo.filetype].start_comment
+end
+
+---@return string: fill comment
+function M.fill_comment()
+	return M.config.ft[bo.filetype].fill_comment
+end
+
+---@return string: end comment
+function M.end_comment()
+	return M.config.ft[bo.filetype].end_comment
+end
+
+return M
