48ff6a7af9dd042ef25b833113903a6bf1aa0a86
Vinicius Oliveira <vinicius@myriad.codes>
2023-01-14 16:39:45 -0300

    fix: fixes issue where single char delimeters would format in a bad way

--- lua/header42/header.lua
+++ lua/header42/header.lua
@@ -6,7 +6,7 @@
 --   By: vgoncalv <vgoncalv@student.42sp.org.br>    +#+  +:+       +#+        --
 --                                                +#+#+#+#+#+   +#+           --
 --   Created: 2021/09/12 20:57:30 by vgoncalv          #+#    #+#             --
---   Updated: 2023/01/13 18:35:46 by vgoncalv         ###   ########.fr       --
+--   Updated: 2023/01/14 16:38:49 by vgoncalv         ###   ########.fr       --
 --                                                                            --
 -- ************************************************************************** --
 
@@ -24,17 +24,17 @@ local config = require("header42.config")
 local delimeters = require("header42.delimeters")
 
 local TEMPLATE = {
-	"**************************************************************************",
-	"                                                                          ",
-	"                                                       :::      ::::::::  ",
-	"  @FILENAME..................................        :+:      :+:    :+:  ",
-	"                                                   +:+ +:+         +:+    ",
-	"  By: @AUTHOR................................    +#+  +:+       +#+       ",
-	"                                               +#+#+#+#+#+   +#+          ",
-	"  Created: @CREATED_AT........ by @CREATED_BY       #+#    #+#            ",
-	"  Updated: @UPDATED_AT........ by @UPDATED_BY      ###   ########.fr      ",
-	"                                                                          ",
-	"**************************************************************************",
+	"********************************************************************************",
+	"*                                                                              *",
+	"*                                                         :::      ::::::::    *",
+	"*    @FILENAME..................................        :+:      :+:    :+:    *",
+	"*                                                     +:+ +:+         +:+      *",
+	"*    By: @AUTHOR................................    +#+  +:+       +#+         *",
+	"*                                                 +#+#+#+#+#+   +#+            *",
+	"*    Created: @CREATED_AT........ by @CREATED_BY       #+#    #+#              *",
+	"*    Updated: @UPDATED_AT........ by @UPDATED_BY      ###   ########.fr        *",
+	"*                                                                              *",
+	"********************************************************************************",
 }
 
 ---@alias HeaderData Header
@@ -99,7 +99,13 @@ function M:lines()
 		end)
 
 		-- Add the header delimeters to the line and return it
-		return string.format("%s %s %s", self.delimeters[1], line, self.delimeters[2])
+		local delimeter_len = string.len(self.delimeters[1])
+		return string.format(
+			"%s %s %s",
+			self.delimeters[1],
+			string.sub(line, delimeter_len + 2, line:len() - delimeter_len - 1),
+			self.delimeters[2]
+		)
 	end, TEMPLATE)
 
 	-- Append an empty line to the header
@@ -107,17 +113,37 @@ function M:lines()
 	return lines
 end
 
+-- TODO: find a better way to check if lines compose a header
+
 ---@type fun(lines: string[]): boolean
 local function lines_are_header(lines)
 	if #lines < 11 then
 		return false
 	end
 
+	local lines_delimeter = { string.match(lines[1], "^([^%s]+) .- ([^%s]+)$") }
+
+	if not #lines_delimeter then
+		return false
+	end
+
+	local delimeter_len = lines_delimeter[1]:len()
 	for lineno = 1, #lines do
-		local line = string.gsub(lines[lineno], "[^%s]+ (.*) [^%s]+", "%1")
+		local line = lines[lineno]
+
+		if not string.match(line, string.format("^%s .- %s$", unpack(lines_delimeter))) then
+			return false
+		end
+
+		local template_line = string.format(
+			"%s %s %s",
+			lines_delimeter[1],
+			string.sub(TEMPLATE[lineno], delimeter_len + 2, line:len() - delimeter_len - 1),
+			lines_delimeter[2]
+		)
 
 		-- Escape punctuation chars
-		local template_line = string.gsub(TEMPLATE[lineno] or "", "[+*]", "%%%1")
+		template_line = string.gsub(template_line or "", "[+*]", "%%%1")
 
 		local range = { string.find(template_line, "@[%w_]*%.*") }
 		local has_annotations = range ~= 0
@@ -129,6 +155,8 @@ local function lines_are_header(lines)
 		end
 
 		if not string.match(line, template_line) then
+			print(line)
+			print(template_line)
 			return false
 		end
 	end
@@ -136,10 +164,12 @@ local function lines_are_header(lines)
 	return true
 end
 
+-- TODO: find a better way to extract information from a buffer
+
 ---Retrieves the data from an existing École 42 Header
 ---@type fun(bufnr: integer): Header?
 function M.frombuffer(bufnr)
-	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, 12, false)
+	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, 11, false)
 
 	local ok = pcall(vim.validate, { lines = { lines, lines_are_header, "École 42 header" } })
 
@@ -153,26 +183,32 @@ function M.frombuffer(bufnr)
 			string.match(lines[1], "^([^%s]+) .- ([^%s]+)$"),
 		},
 	}
-	local escaped_delimeters = {
-		string.gsub(data.delimeters[1], "[+*]", "%%%1"),
-		string.gsub(data.delimeters[2], "[+*]", "%%%1"),
-	}
+	local delimeter_len = string.len(data.delimeters[1])
 
 	for lineno = 1, #TEMPLATE do
-		-- We add the delimeters from the header to the substitution pattern so we
-		-- can remove them
-		local line = string.gsub(
-			lines[lineno],
-			string.format("%s (.*) %s", unpack(escaped_delimeters)),
-			"%1"
+		local line = lines[lineno]
+		local template_line = string.format(
+			"%s %s %s",
+			data.delimeters[1],
+			string.sub(TEMPLATE[lineno], delimeter_len + 2, line:len() - delimeter_len - 1),
+			data.delimeters[2]
 		)
-		local template_line = TEMPLATE[lineno]
 
 		for annotation, key in string.gmatch(template_line, "(@([%w_]+)%.*)") do
 			key = key:lower()
 			local range = { string.find(template_line, annotation) }
+			local content = vim.fn.trim(string.sub(line, unpack(range)))
 
-			data[key] = vim.fn.trim(string.sub(line, unpack(range)))
+			if key == "author" then
+				local login, email = string.match(content, "^([^%s]+) <([^%s]+)>")
+
+				data[key] = {
+					login = login,
+					email = email,
+				}
+			else
+				data[key] = content
+			end
 		end
 	end
 
